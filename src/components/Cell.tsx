2ecaff6215c2c18e3deeef7ab4c3910aa4e5ddc3
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2023-05-20 01:26:52 +0300

    feat: add basic cell logic

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,31 +1,64 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const StyledCell = styled('li')`
   color: black;
   border: 1px solid black;
   background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;
-export const StyledPressedCell = styled(StyledCell)`
+export const StyledPressedLeftCell = styled(StyledCell)`
   background-color: #9f9f9f;
 `;
 
-function Cell() {
-  const [isPressed, setIsPressed] = useState(false);
+export const StyledPressedRightCell = styled(StyledCell)`
+  background-color: orange;
+`;
 
-  return isPressed ? (
-    <StyledPressedCell
-      onClick={() => {
-        setIsPressed(!isPressed);
-      }}
-    />
-  ) : (
-    <StyledCell
-      onClick={() => {
-        setIsPressed(!isPressed);
-      }}
-    />
-  );
+function Cell() {
+  const [isPressed, setIsPressed] = useState('false');
+
+  switch (isPressed) {
+    case 'left':
+      return (
+        <StyledPressedLeftCell
+          onClick={() => {
+            setIsPressed('false');
+          }}
+        />
+      );
+    case 'right':
+      return (
+        <StyledPressedRightCell
+          onClick={() => {
+            setIsPressed('false');
+          }}
+          onContextMenu={(event) => {
+            event.preventDefault();
+            setIsPressed('false');
+          }}
+        >
+          <FontAwesomeIcon icon={faQuestion} />
+        </StyledPressedRightCell>
+      );
+    default:
+      return (
+        <StyledCell
+          onClick={() => {
+            setIsPressed('left');
+          }}
+          onContextMenu={(event) => {
+            event.preventDefault();
+            setIsPressed('right');
+          }}
+        />
+      );
+  }
 }
 
 export default Cell;
