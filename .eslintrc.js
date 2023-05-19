1ae1429ee1a64d7b6e941db76c9c1b606cac8a6a
SavitskayaKseniya22 <77901301+SavitskayaKseniya22@users.noreply.github.com>
2023-05-19 18:22:06 +0300

    fix: config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,23 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'airbnb-typescript', "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb/hooks', 'airbnb-typescript', 'prettier'],
 
-    overrides: [
-    ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        parser: "@typescript-eslint/parser",
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: '../minesweeper/tsconfig.json'
+    ecmaFeatures: {
+      jsx: true,
     },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        "prettier"
-
-    ],
-    rules: {
-        "@typescript-eslint/no-explicit-any": "error",
-    },
-
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: '../minesweeper/tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+  },
 };
